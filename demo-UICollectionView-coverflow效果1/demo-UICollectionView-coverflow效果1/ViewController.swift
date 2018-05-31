1091d550cd5cb789ba498da3ce08d94b6b59aeeb
qj.huang <>
2018-05-31 15:47:38 +0800

    first commit

---
+++ demo-UICollectionView-coverflow效果1/demo-UICollectionView-coverflow效果1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  demo-UICollectionView-coverflow效果1
+//
+//  Created by qj.huang on 2018/5/31.
+//  Copyright © 2018年 qjmac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "效果1"
+        view.backgroundColor = UIColor.brown
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ demo-普通动画/demo-普通动画/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  demo-普通动画
+//
+//  Created by qj.huang on 2018/5/31.
+//  Copyright © 2018年 qjmac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "效果2"
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
